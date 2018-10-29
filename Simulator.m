28774389376fb03b4b934d8f5bf4a11e07d1092e
rcm2146 <32101803+rcm2146@users.noreply.github.com>
2018-10-29 18:44:16 -0400

    Update simulator
    Adding properties and constructor

--- Simulator.m
+++ Simulator.m
@@ -1,24 +1,41 @@
-
-classdef Simulator
-    %Robot Summary of this class goes here
+classdef simulator
+    %UNTITLED Summary of this class goes here
     %   Detailed explanation goes here
     
     properties
-        Property1
+        bots % Array of type robot1. Represents the bodies in the system
+        dt % Double. Length of one time-step (in seconds)
+        g % Double array. Gravitational acceleration (m/s^2)
+%       rho % Double. Global velocity damping parameter (0<p<1)
     end
     
     methods
-        function obj = untitled2(inputArg1,inputArg2)
-            %UNTITLED2 Construct an instance of this class
+        function obj = simulator(bots, dt, g, rho)
+            %SIMULATOR Construct an instance of simulator class
             %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
+            if nargin>0
+                obj.bots = bots;
+                if nargin>1
+                    obj.dt = dt;
+                    if nargin >2
+                        obj.g = g;
+                        if nargin>3
+                            obj.rho = rho;
+                            if nargin>4
+                                disp('Too many arguments for simulator');
+                            end
+                        end
+                    end
+                end
+            end
+                
         end
         
-        function outputArg = method1(obj,inputArg)
+        function outputArg = step(obj,inputArg)
             %METHOD1 Summary of this method goes here
             %   Detailed explanation goes here
             outputArg = obj.Property1 + inputArg;
-            
         end
     end
 end
+
