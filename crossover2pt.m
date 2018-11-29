ebec1075828a82905bdcfe2a359e233711f3c8cc
rcm2146 <rcm2146@columbia.edu>
2018-11-29 08:49:48 -0500

    Improve crossover, got rid of redundant evaluations

--- crossover2pt.m
+++ crossover2pt.m
@@ -1,5 +1,5 @@
 function [children] = crossover2pt(parents)
-
+% This crossover function takes a set of bots and returns a set of bots
 p = length(parents);
 cut = sort([randi(15, p, 1), randi(15, p, 1)],2);
 children = starfish_robot.empty(0,length(parents));
@@ -7,8 +7,6 @@ children = starfish_robot.empty(0,length(parents));
 for i = 1:2:length(parents)-1
     parA = parents(i).gene;
     parB = parents(i+1).gene;
-    i
-    cut(i,:)
     geneC = [parA(1:cut(i,1)); parB(cut(i,1)+1: cut(i,2)); parA(cut(i,2)+1:end)];
     geneD = [parB(1:cut(i,1)); parA(cut(i,1)+1: cut(i,2)); parB(cut(i,2)+1:end)];
 
---
+++ crossover2pt_gene.m
@@ -0,0 +1,30 @@
+function [children] = crossover2pt_gene(parents)
+% This crossover function takes and returns genes, not bots
+p = size(parents,2);
+children = zeros(15,p);
+cut = sort([randi(15, p, 1), randi(15, p, 1)],2);
+% children = starfish_robot.empty(0,length(parents));
+
+for i = 1:2:length(parents)-1
+%     parA = parents(i).gene;
+%     parB = parents(i+1).gene;
+    parA = parents(i);
+    parB = parents(i+1);
+    geneC = [parA(1:cut(i,1)); parB(cut(i,1)+1: cut(i,2)); parA(cut(i,2)+1:end)];
+    geneD = [parB(1:cut(i,1)); parA(cut(i,1)+1: cut(i,2)); parB(cut(i,2)+1:end)];
+
+    %Child is placed in same index as more similar parent
+    if cut(i,2)-cut(i,1) < 8
+%         children(i) = starfish_robot(geneC);
+%         children(i+1) = starfish_robot(geneD);
+        children(i) = geneC;
+        children(i+1) = geneD;
+    else
+%         children(i) = starfish_robot(geneD);
+%         children(i+1) = starfish_robot(geneC);
+        children(i) = geneD;
+        children(i+1) = geneC;
+    end
+end 
+
+end
